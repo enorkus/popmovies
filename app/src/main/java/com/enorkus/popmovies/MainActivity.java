31ba14596fb5d2bdd17dd026c12571710b58e0d1
enorkus <eitvys@gmail.com>
2018-03-29 21:36:18 +0300

    offline handling

--- app/src/main/java/com/enorkus/popmovies/MainActivity.java
+++ app/src/main/java/com/enorkus/popmovies/MainActivity.java
@@ -1,10 +1,19 @@
 package com.enorkus.popmovies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.enorkus.popmovies.adapter.MovieAdapter;
 import com.enorkus.popmovies.asynctask.MovieDBQueryTask;
@@ -31,6 +40,10 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
     protected GridView GVmoviePosters;
     @BindView(R.id.bottomNavigation)
     protected BottomNavigationView bottomNavigation;
+    @BindView(R.id.LLnoInternetConnection)
+    protected LinearLayout LLnoInternetConnection;
+    @BindView(R.id.reloadBtn)
+    protected Button reloadButton;
 
     private MovieDBQueryTask queryTask;
 
@@ -39,19 +52,35 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        createLayout(savedInstanceState);
+    }
 
-        if(savedInstanceState != null) {
-            bottomNavigation.setSelectedItemId(savedInstanceState.getInt(BOTTOM_NAVIGATION_SELECTED_ID));
-            getAsyncResponseOnFinish(savedInstanceState.getParcelableArrayList(SELECTED_MOVIES));
-            MainActivityStateHolder.sortOrder = MainActivityStateHolder.MovieSortOrder.valueOf(savedInstanceState.getString(SELECTED_SORT_ORDER));
+    private void createLayout(Bundle savedInstanceState) {
+        if(isOnline(this)) {
+            if(savedInstanceState != null && savedInstanceState.getParcelableArrayList(SELECTED_MOVIES) != null) {
+                bottomNavigation.setSelectedItemId(savedInstanceState.getInt(BOTTOM_NAVIGATION_SELECTED_ID));
+                getAsyncResponseOnFinish(savedInstanceState.getParcelableArrayList(SELECTED_MOVIES));
+                MainActivityStateHolder.sortOrder = MainActivityStateHolder.MovieSortOrder.valueOf(savedInstanceState.getString(SELECTED_SORT_ORDER));
+            } else {
+                queryTask = new MoviesQueryTask(this);
+                queryTask.execute(ConnectionUtils.buildPopularMoviesURL());
+                MainActivityStateHolder.sortOrder = MainActivityStateHolder.MovieSortOrder.POPULAR;
+            }
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigation.getLayoutParams();
+            layoutParams.setBehavior(new BottomNavigationViewBehavior());
+            bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationItemSelectListener(this, GVmoviePosters));
         } else {
-            queryTask = new MoviesQueryTask(this);
-            queryTask.execute(ConnectionUtils.buildPopularMoviesURL());
-            MainActivityStateHolder.sortOrder = MainActivityStateHolder.MovieSortOrder.POPULAR;
+            GVmoviePosters.setVisibility(View.GONE);
+            LLnoInternetConnection.setVisibility(View.VISIBLE);
+            reloadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                }
+            });
         }
-        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) bottomNavigation.getLayoutParams();
-        layoutParams.setBehavior(new BottomNavigationViewBehavior());
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationItemSelectListener(this, GVmoviePosters));
     }
 
     @Override
@@ -63,8 +92,16 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(BOTTOM_NAVIGATION_SELECTED_ID, bottomNavigation.getSelectedItemId());
-        outState.putParcelableArrayList(SELECTED_MOVIES, MainActivityStateHolder.movies);
-        outState.putString(SELECTED_SORT_ORDER, MainActivityStateHolder.sortOrder.name());
+        if(isOnline(this)) {
+            outState.putInt(BOTTOM_NAVIGATION_SELECTED_ID, bottomNavigation.getSelectedItemId());
+            outState.putParcelableArrayList(SELECTED_MOVIES, MainActivityStateHolder.movies);
+            outState.putString(SELECTED_SORT_ORDER, MainActivityStateHolder.sortOrder != null ? MainActivityStateHolder.sortOrder.name() : null);
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
     }
 }
